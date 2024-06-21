cffcb293ab7a20174811e7db350f4ea9c3ed2c7c
Nurdaulet <147368298+nurdaulet00@users.noreply.github.com>
2024-06-22 01:27:21 +0500

    Add

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,7 +8,12 @@ from loader import dp
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup=mian_keyboard)
+    if message.from_user.language_code == "ru":
+        await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup=mian_keyboard)
+    elif message.from_user.language_code == "kz":
+        await message.answer(f'Сәлем, {message.from_user.full_name}!', reply_markup=mian_keyboard)
+    else:
+        await message.answer(f'Hello, {message.from_user.full_name}!', reply_markup=mian_keyboard)
 
 
 # @dp.callback_query_handler(command_callback.filter(command="start"))
