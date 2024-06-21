79799e7558291060e4f6918cd92ba1f76fb69fa0
ALISHER KSHTYKENOV <alisher_kshtykenov@icloud.com>
2024-06-21 22:37:20 +0600

    Commit

---
+++ data/config.py
@@ -0,0 +1,28 @@
+# import os
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+
+# BOT TOKEN моего telegram бота.
+BOT_TOKEN = "7014783595:AAGrOaSE5D4lfxkUEp9EoK6yWd1gAxdGMWg"
+
+# Id моего чата.
+admins = [
+    1478600655
+]
+
+# API для моего USER id.
+api_id = 29168870
+api_hash = "9e974797a9b1880bd44c800d5fedfdbd"
+
+# ip = os.getenv("ip")
+#
+# aiogram_redis = {
+#     'host': ip,
+# }
+#
+# redis = {
+#     'address': (ip, 6379),
+#     'encoding': 'utf8'
+# }
