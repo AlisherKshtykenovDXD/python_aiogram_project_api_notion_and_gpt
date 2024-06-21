d6b4ae22ef039207f9e42e54e51917dc68e92070
ALISHER KSHTYKENOV <alisher_kshtykenov@icloud.com>
2024-06-22 02:23:46 +0600

    Commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,5 +1,8 @@
+import logging
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from aiogram.types import CallbackQuery
 
 from keyboards.inline.callback_datas import command_callback
 from keyboards.inline.start_buutons import mian_keyboard
@@ -11,5 +14,14 @@ async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!', reply_markup=mian_keyboard)
 
 
-# @dp.callback_query_handler(command_callback.filter(command="start"))
-# async def
+@dp.callback_query_handler(command_callback.filter(name_command="start"))
+async def menu_command_start(call: CallbackQuery, callback_data: dict):
+    await call.answer(cache_time=60)
+    logging.info(f"callback_data = {call.data}")
+    logging.info(f"callback_data dict = {callback_data}")
+    arguments = callback_data.get("arguments")
+
+    if arguments == "":
+        await call.message.answer(f"Какая то там реакция из за нажатия кнопки: {arguments}")
+    else:
+        await call.message.answer(f"Какая то там реакция из за нажатия кнопки: {arguments}")
